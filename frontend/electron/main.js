0b898d9c7c821f74214b0aa44cebd0357a3aeb50
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2025-10-14 21:21:55 -0300

    feat auto update app

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -1,11 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { autoUpdater, AppUpdater } from 'electron-updater';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let mainWindow;
+
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -33,6 +38,23 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
+autoUpdater.on("update-available", (info) => {
+  curWindow.showMessage(`Update available. Current version ${app.getVersion()}`);
+  let pth = autoUpdater.downloadUpdate();
+  curWindow.showMessage(pth);
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  curWindow.showMessage(`No update available. Current version ${app.getVersion()}`);
+});
+
+/*Download Completion Message*/
+autoUpdater.on("update-downloaded", (info) => {
+  curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
+});
+
+
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  autoUpdater.checkForUpdates();
 });
